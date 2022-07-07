80c9bec7a04252e0f603b7b2908381fe1bb435bd
raen2000 <raenjs2000@gmail.com>
2022-07-07 13:45:44 +0800

    Updated HTML Files

--- HTML Files/js/generate-pdf.js
+++ HTML Files/js/generate-pdf.js
@@ -2,7 +2,7 @@ function generatePDF () {
 	const element = document.getElementById("element");
 
 	html2pdf(element, {
-  		margin:       10,
+  		margin:       0,
   		filename:     'grades.pdf',
   		image:        { type: 'jpeg', quality: 0.98 },
   		html2canvas:  { scale: 4, logging: true, letterRendering: true},
