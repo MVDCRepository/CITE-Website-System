52762575ffdc4d903dd0e9be1ced7fccdeb2c274
raen2000 <raenjs2000@gmail.com>
2022-07-12 01:31:20 +0800

    Updated HTML Files

--- HTML Files/js/generate-pdf.js
+++ HTML Files/js/generate-pdf.js
@@ -1,5 +1,7 @@
 function generatePDF () {
 	const element = document.getElementById("element");
+	
+	element.style.display = "block";
 
 	html2pdf(element, {
   		margin:       0,
@@ -7,6 +9,8 @@ function generatePDF () {
   		image:        { type: 'jpeg', quality: 0.98 },
   		html2canvas:  { scale: 4, logging: true, letterRendering: true},
   		jsPDF:        { unit: 'mm', format: 'Legal', orientation: 'portrait' }
+
 });
 	
+	element.style.display = "none";
 }
